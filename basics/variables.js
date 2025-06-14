94d885a192acc96c99690d6b509387f979b7e850
Saif Ali Khan <k.saifalikhan8@gmail.com>
2025-06-14 14:07:09 +0000

    vari

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const account_height = 5.5;
+let account_phone = "9032604803";
+var account_email = "k.saifalikhan8@gmail.com";
+
+account_state= "KA";
+
+// account_height=5.8;
+
+let account_name;
+
+console.log(account_height);
+
+console.table([account_email, account_height, account_phone, account_name, account_state]);
